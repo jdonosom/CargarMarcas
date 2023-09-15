3ea5426a11a8df4d203e5aee290c527b91b9774c
Pepe Luna <pepeluna27@hotmail.com>
2023-09-15 00:19:15 -0300

    Carga Inicial

---
+++ CargarMarcas/FrmCargaMarca.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace CargarMarcas
+{
+    partial class FrmCargaMarca
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmCargaMarca));
+            lstArchivos = new ListView();
+            columnHeader1 = new ColumnHeader();
+            columnHeader2 = new ColumnHeader();
+            panel1 = new Panel();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            btnLimpiar = new Button();
+            btnCargar = new Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // lstArchivos
+            // 
+            lstArchivos.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2 });
+            lstArchivos.Location = new Point(9, 110);
+            lstArchivos.Name = "lstArchivos";
+            lstArchivos.Size = new Size(268, 438);
+            lstArchivos.TabIndex = 4;
+            lstArchivos.UseCompatibleStateImageBehavior = false;
+            lstArchivos.View = View.Details;
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "Archivo";
+            columnHeader1.Width = 120;
+            // 
+            // columnHeader2
+            // 
+            columnHeader2.Text = "Reg.";
+            columnHeader2.Width = 80;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(800, 66);
+            panel1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(63, 32);
+            label1.Name = "label1";
+            label1.Size = new Size(206, 25);
+            label1.TabIndex = 1;
+            label1.Text = "Carga de marcaciones";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(9, 9);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(48, 48);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // btnLimpiar
+            // 
+            btnLimpiar.Image = (Image)resources.GetObject("btnLimpiar.Image");
+            btnLimpiar.Location = new Point(9, 72);
+            btnLimpiar.Name = "btnLimpiar";
+            btnLimpiar.Size = new Size(32, 32);
+            btnLimpiar.TabIndex = 6;
+            btnLimpiar.UseVisualStyleBackColor = true;
+            btnLimpiar.Click += btnLimpiar_Click;
+            // 
+            // btnCargar
+            // 
+            btnCargar.Image = (Image)resources.GetObject("btnCargar.Image");
+            btnCargar.Location = new Point(245, 72);
+            btnCargar.Name = "btnCargar";
+            btnCargar.Size = new Size(32, 32);
+            btnCargar.TabIndex = 7;
+            btnCargar.UseVisualStyleBackColor = true;
+            btnCargar.Click += btnCargar_Click;
+            // 
+            // FrmCargaMarca
+            // 
+            AllowDrop = true;
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 560);
+            Controls.Add(btnCargar);
+            Controls.Add(btnLimpiar);
+            Controls.Add(lstArchivos);
+            Controls.Add(panel1);
+            Name = "FrmCargaMarca";
+            Text = "FrmCargaMarca";
+            DragDrop += FrmCargaMarca_DragDrop;
+            DragEnter += FrmCargaMarca_DragEnter;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ListView lstArchivos;
+        private Panel panel1;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private PictureBox pictureBox2;
+        private Button btnLimpiar;
+        private Button btnCargar;
+    }
+}
---
+++ CargarMarcas/FrmCargaMarca.cs
@@ -0,0 +1,111 @@
+﻿using System.Data;
+using System.Globalization;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace CargarMarcas
+{
+    public partial class FrmCargaMarca : Form
+    {
+        public FrmCargaMarca()
+        {
+            InitializeComponent();
+
+            System.Windows.Forms.ToolTip toolTip = new System.Windows.Forms.ToolTip();
+
+            toolTip.SetToolTip(this.btnLimpiar, "Limpiar archivos");
+            toolTip.SetToolTip(this.btnCargar, "Cargar archivos");
+
+            btnCargar.Enabled = false;
+        }
+
+        private void FrmCargaMarca_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+        }
+
+        private void FrmCargaMarca_DragDrop(object sender, DragEventArgs e)
+        {
+            // recuperar los datos del archivo que fue arrastrado
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+
+            files = files.OrderBy(x => x).ToArray();
+            foreach (var file in files)
+            {
+                try
+                {
+                    // MessageBox.Show(file);
+                    //
+                    PreProcesar(file);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void PreProcesar(string file)
+        {
+            // lstArchivos.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+
+            int NroRegistros = 0;
+            NroRegistros = CuentaNroRegistros(file);
+
+            ListViewItem item = new ListViewItem();
+
+            item.Text = Path.GetFileName(file);
+            var nombrearchivo = item.Text.Split('_');
+            DateTime fecha;
+            if (!nombrearchivo[0].ToLower().Equals("marcas"))
+                throw new Exception("Error en nombre de archivo");
+
+            if (!DateTime.TryParseExact($"{nombrearchivo[1].Split('.')[0]}", "yyyyMMdd",
+                CultureInfo.InvariantCulture,
+                DateTimeStyles.None, out fecha))
+                throw new Exception("La fecha del archivo no es valida");
+
+            item.SubItems.Add(NroRegistros.ToString());
+            lstArchivos.Items.Add(item);
+
+            lstArchivos.Columns[0].Width = item.Text.Length * 8;
+
+            btnCargar.Enabled = lstArchivos.Items.Count > 0 ? true : false;
+
+        }
+
+        private int CuentaNroRegistros(string file)
+        {
+            string[] lines =
+                File.ReadAllLines(file);
+            int NroRegistros = lines.Count();
+            int nRegistros = 0;
+            foreach (var line in lines)
+            {
+                var data = line.Split(',');
+                if (data.Length.Equals(4))
+                {
+                    try
+                    {
+                        int.Parse(data[0]);
+                        nRegistros++;
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+                }
+            }
+            return (nRegistros == NroRegistros ? NroRegistros : nRegistros);
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            lstArchivos.Items.Clear();
+        }
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
